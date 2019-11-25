4f3789e782e600b944dfca018fa4a92dc0ed1efb
Vida96 <nikolavidovic47@yahoo.com>
2019-11-25 11:23:12 +0100

    Dodano jos funkcionalnosti, nakon ovoga izvrsiti testiranje, pa nastaviti (sutra) sa radom.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
 
@@ -14,12 +16,50 @@ public class Main {
         return null;
     }
 
+    //generisanje pocetne populacije
     private static List<Double> generateInitialPopulation(Integer sizeOfPopulation) {
         return new Random().doubles(LOWER_BOUND,HIGHER_BOUND).limit(sizeOfPopulation).boxed().collect(Collectors.toList());
     }
 
     private static List<Double> generateRandomNumbers(Integer sizeOfPopulation) {
         return generateInitialPopulation(sizeOfPopulation);
+    }
+
+    //broj bita za kodovanje
+    private static Integer computeSizeOfBitsForCoding() {
+        Integer p = 2;
+        return (int)Math.ceil(log2((HIGHER_BOUND - LOWER_BOUND) * Math.pow(10, p) + 1)); //n = 𝑙𝑜𝑔2[(𝐺𝑔 − 𝐺𝑑)10𝑝 + 1]
+    }
+
+    public static double log2(double n)
+    {
+        return (Math.log(n) / Math.log(2));
+    }
+
+    //decimalno kodovanje koordinata x, y u bd
+    private static List<Integer> codeCoordinatesToDecimal(List<Double> cooridantes, Integer sizeOfBitsForCoding) {
+        List<Integer> decimalCodedCoordinates = new ArrayList<>(cooridantes.size());
+        for(Double coordinate : cooridantes)
+            decimalCodedCoordinates.add((int)Math.ceil((coordinate - LOWER_BOUND) / (HIGHER_BOUND - LOWER_BOUND) * (Math.pow(2, sizeOfBitsForCoding) - 1)));  // bd = [(𝑥 − 𝐺𝑑 / Gg - Gd) * (2^n - 1)]
+        return decimalCodedCoordinates;
+    }
+
+    //binarno kodovanje koordinata x, y
+    private static List<String> codeCoordinatesToBinary(List<Integer> decimalCooridantes, Integer sizeOfBitsForCoding) {
+        List<String> binaryCodedCoordinates = new ArrayList<>(decimalCooridantes.size());
+        for(Integer coordinate : decimalCooridantes)
+          binaryCodedCoordinates.add(String.format("%" + sizeOfBitsForCoding.toString() + "s", Integer.toBinaryString(coordinate)).replace(' ', '0'));
+        return binaryCodedCoordinates;
+    }
+
+    //racunanje z = f(x, y)
+    private static List<Double> computeFunctionOfCoordinates(List<Double> cooridantesX, List<Double> cooridantesY) {
+     List<Double> computedFunction = new ArrayList<Double>(cooridantesX.size());
+     Stream.concat(cooridantesX.stream(), cooridantesY.stream()).forEachOrdered(str -> {
+            // compute function
+        // computedFunction.add(izracunata f-ja)
+     });
+     return computedFunction;
     }
 
     public static void main(String[] args){
@@ -39,12 +79,18 @@ public class Main {
         List<Double> randomNumbers = generateRandomNumbers(sizeOfPopulation);
 
         //broj bita potrebnih za kodovanje (n)
+        Integer sizeOfBitsForCoding = computeSizeOfBitsForCoding();
 
         //decimalno kodovanje (bd)
+        List<Integer> decimalCodedCoordinatesX = codeCoordinatesToDecimal(cooridantesX, sizeOfBitsForCoding);
+        List<Integer> decimalCodedCoordinatesY = codeCoordinatesToDecimal(cooridantesY, sizeOfBitsForCoding);
 
         //pretvaranje u binarni kod i ispis (Tabela 2. u PDF-u) - pocetna populacija
+        List<String> binaryCodedCoordinatesX = codeCoordinatesToBinary(decimalCodedCoordinatesX, sizeOfBitsForCoding);
+        List<String> binaryCodedCoordinatesY = codeCoordinatesToBinary(decimalCodedCoordinatesY, sizeOfBitsForCoding);
 
         //racunanje f(x), ff(x) i ispis (Tabela 3. u PDF-u) - ocjena pocetne populacije
+        List<Double> computedFunctionZ = computeFunctionOfCoordinates(cooridantesX, cooridantesY);
 
         //racunanje ocjene cijele pocetne populacije (F)
 
